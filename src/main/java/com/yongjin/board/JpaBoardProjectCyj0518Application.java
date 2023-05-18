f9eb7a29ec92231b764445205766247e6e4610c1
7CYJ <jojo3528@gmail.net>
2023-05-18 16:10:42 +0900

    entity 설정 완료

---
+++ src/main/java/com/yongjin/board/JpaBoardProjectCyj0518Application.java
@@ -0,0 +1,13 @@
+package com.yongjin.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardProjectCyj0518Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardProjectCyj0518Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yongjin/board/JpaBoardProjectCyj0518ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongjin.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardProjectCyj0518ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
